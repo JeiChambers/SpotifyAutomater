7deafc2c1ee98195dce3d4af5533b3c758c3a6af
Johnny L. Chambers <john.chambers@cloudimperiumgames.com>
2020-03-12 19:10:07 -0500

    Initial Commit

---
+++ auto_spotify.py
@@ -0,0 +1,29 @@
+"""
+Step 1: Log into YouTube
+Step 2: Grab Our Liked Videos
+Step 3: Create a New Playlist
+Step 4: Search for the Song
+Step 5: Add this song into the new Spotify Playlist
+"""
+
+class CreatePlaylist:
+    def __init__(self):
+        pass
+
+    #Step 1: Login to YT
+    def get_youtube_client(self):
+        pass
+    
+    def get_liked_videos(self):
+        pass
+
+    def create_playlist(self):
+        pass
+
+    def get_spotify_url(self):
+        pass
+
+    def add_song_to_playlist(self):
+        pass
+
+    
